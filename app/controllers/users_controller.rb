d3b7daca003c8a632171ff5ce1f03784e3d99675
Marcelo Soares Souza <marcelo@riseup.net>
2016-10-08 12:47:19 -0300

    Adicionado CRUD Básico para Usuários

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,13 @@ class UsersController < ApplicationController
       format.json { render json: @users }
     end
   end
+
+  def show
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @user }
+    end
+  end
 end
