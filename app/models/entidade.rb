74c9c16ac19d9214a4ca0e7a2d919d0758989ce6
Marcelo Soares Souza <marcelo@riseup.net>
2016-10-09 10:35:50 -0300

    Pequenas Correções em Entidades. Melhorias em Permissionamento Básico

--- app/models/entidade.rb
+++ app/models/entidade.rb
@@ -1,3 +1,6 @@
 class Entidade < ApplicationRecord
   belongs_to :user
+
+  validates :nome, :presence => true, :uniqueness => { :case_sensitive => false }, length: { minimum: 3 }
+  validates :cnpj, :presence => true, :uniqueness => { :case_sensitive => false }, length: { minimum: 5 }
 end
