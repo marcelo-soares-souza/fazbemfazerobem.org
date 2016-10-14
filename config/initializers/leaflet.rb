c20364fd3cfdfb4979971a2b1b736a1f95777c42
Marcelo Soares Souza <marcelo@riseup.net>
2016-10-14 18:20:21 -0300

    Correção na URL do OpenStreetMap com HTTPS

--- config/initializers/leaflet.rb
+++ config/initializers/leaflet.rb
@@ -1,4 +1,4 @@
-Leaflet.tile_layer = "http://{s}.osm.org/{z}/{x}/{y}.png"
+Leaflet.tile_layer = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 Leaflet.attribution = "Projeto Doar"
 Leaflet.max_zoom = 18
-Leaflet.subdomains = ['tile']
+Leaflet.subdomains = ['a']
