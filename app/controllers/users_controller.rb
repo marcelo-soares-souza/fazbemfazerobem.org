280d319492cc469313de0cd1bfeff6d8130d86a9
Marcelo Soares Souza <marcelo@riseup.net>
2016-12-15 09:11:14 -0200

    Correção no Retorno REST JSON de Tipos de Usuários

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,11 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @users }
+    end
   end
 
   def show
